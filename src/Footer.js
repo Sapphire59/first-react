e4791dd2895cd6b7d551e0d38975293f10557a64
Deborah <liz.joy43@gmail.com>
2024-11-30 11:40:42 -0700

    added footer with links and updated CSS

--- src/Footer.js
+++ src/Footer.js
@@ -12,14 +12,22 @@ function Footer() {
           rel="noopener noreferrer"
         >
           Deborah Chacko
-        </a>
-        and is{" "}
+        </a>{" "}
+        is open-sourced on{" "}
         <a
           href="https://github.com/Sapphire59/first-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          Github{" "}
+        </a>
+        and is hosted on{" "}
+        <a
+          href="https://deborah-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
         </a>
       </p>
     </div>

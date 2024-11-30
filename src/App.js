eb2ef401773bd79a7252630e5664d91db3c79fba
Deborah <liz.joy43@gmail.com>
2024-11-30 10:07:36 -0700

    added search engine,

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>I am learning React</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
